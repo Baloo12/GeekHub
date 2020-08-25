1634a9678bfea03532edaf0c1e4129c925064e9e
Oleksandr Antosiev <oleksandr.antosiev@wolterskluwer.com>
2020-08-25 22:45:31 +0300

    swagger register

--- src/GamesHub/GamesHub.Web/Startup.cs
+++ src/GamesHub/GamesHub.Web/Startup.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 namespace GamesHub.Web
 {
     using AutoMapper;
@@ -10,60 +12,47 @@ namespace GamesHub.Web
 
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
-    using Microsoft.AspNetCore.Http;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
-    using Microsoft.OpenApi.Models;
 
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
-
-            app.UseSwagger(c =>
-                {
-                    c.SerializeAsV2 = true;
-                });
-            app.UseSwaggerUI(x =>
-                {
-                    x.SwaggerEndpoint("/swagger/v1/swagger.json", "GamesHub API V1");
-                });
-
-
-            app.UseRouting();
-
-            app.UseEndpoints(
-                endpoints =>
-                    {
-                        endpoints.MapControllers();
-                    });
-
-
-        }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerGen(s =>
+            {
+                s.SwaggerDoc("v1", new OpenApiInfo { Title = "GamesHub API", Version = "v1" });
+            });
+
             RegisterRepositories(services);
             RegisterServices(services);
 
             services.AddAutoMapper(typeof(Startup));
 
-            services.AddSwaggerGen(
-                c =>
-                    {
-                        c.SwaggerDoc(
-                            "v1",
-                            new OpenApiInfo
-                                {
-                                    Title = "GamesHub API",
-                                    Description = "DotNet Core Api 3 - with swagger",
-                                    Version = "v1"
-                                });
-                    });
+            services.AddControllers(c => c.EnableEndpointRouting = false);
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+
+            app.UseStaticFiles();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(x =>
+            {
+                x.SwaggerEndpoint("/swagger/v1/swagger.json", "GamesHub API V1");
+            });
+
+            app.UseRouting();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "api/[controller]",
+                    defaults: new { controller = "games" });
+            });
         }
 
         private void RegisterRepositories(IServiceCollection services)
