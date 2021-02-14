e8330f7c92580bd57b22a780fc7de4ce5c85219c
Oleksandr Antosiev <oleksandr.antosiev@wolterskluwer.com>
2021-02-14 22:04:14 +0200

    Enrich overview model

--- src/GamesHub/GamesHub.Web/Models/GameOverviewModel.cs
+++ src/GamesHub/GamesHub.Web/Models/GameOverviewModel.cs
@@ -7,6 +7,20 @@
         public Guid Id { get; set; }
 
         public string Name { get; set; }
+        
+        public string Description { get; set; }
+
+        public string Image { get; set; }
+
+        public bool IsFree { get; set; }
+
+        public string ReleaseDate { get; set; }
+
+        public int RequiredAge { get; set; }
+
+        public string Type { get; set; }
+
+        public string Website { get; set; }
 
         public int OverallRank { get; set; }
 
