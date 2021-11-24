ddc0969cfbac70703aa5449dfd261a170809444a
AlexAntosev <alexander.antosev@gmail.com>
2021-11-24 10:01:18 +0200

    Add required attributes for entity fields

--- src/GeekHub.VideoGames/GeekHub.VideoGames.Domain/Entities/VideoGame.cs
+++ src/GeekHub.VideoGames/GeekHub.VideoGames.Domain/Entities/VideoGame.cs
@@ -6,8 +6,10 @@ namespace GeekHub.VideoGames.Domain.Entities
     public class VideoGame
     {
         [Key]
+        [Required]
         public Guid Id { get; set; }
         
+        [Required]
         public string Name { get; set; }
     }
 }
