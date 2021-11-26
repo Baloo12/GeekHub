1ea336285fb097f4106b7baa6ec6472c49fa2097
AlexAntosev <alexander.antosev@gmail.com>
2021-11-26 16:46:48 +0200

    Add generated client

---
+++ src/GeekHub.SteamProvider/GeekHub.SteamProvider.Web.Client/GenerateClient-FromLocal.ps1
@@ -0,0 +1,21 @@
+﻿[CmdletBinding()]
+param()
+ 
+if (!$PSBoundParameters.ContainsKey('ErrorAction')) { $ErrorActionPreference = 'Stop' }
+ 
+$commonParams = @{
+    ErrorAction = $ErrorActionPreference;
+    Verbose = $VerbosePreference -ne 'SilentlyContinue'
+}
+ 
+. "$PSScriptRoot\GenerateClient-Functions.ps1"
+ 
+$endpoint = "http://localhost:5000/swagger/v1/swagger.json"
+$applicationPath = "$PSScriptRoot\..\GeekHub.SteamProvider.Web"
+$clientPath = "$PSScriptRoot\..\GeekHub.SteamProvider.Web.Client\GeekHub.SteamProvider.Web.Client.csproj"
+ 
+$application = Start-WebApp -Endpoint $endpoint -ApplicationPath $applicationPath
+ 
+Generate-Client -Endpoint $endpoint -ClientPath $clientPath @commonParams 
+ 
+Stop-WebApp -Application $application
---
+++ src/GeekHub.SteamProvider/GeekHub.SteamProvider.Web.Client/HttpResponseException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GeekHub.SteamProvider.Web.Client
+{
+    public class HttpResponseException : Exception
+    {
+        protected HttpResponseException(ExceptionResponse response)
+            : base(response?.Message)
+        {
+            Response = response;
+        }
+
+        private ExceptionResponse Response { get; }
+    }
+}
