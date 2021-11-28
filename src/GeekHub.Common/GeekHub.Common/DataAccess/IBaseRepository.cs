69b2d57907a508cfc78555cd3c40cb095daa3a09
AlexAntosev <alexander.antosev@gmail.com>
2021-11-28 17:56:59 +0200

    Add base repo to common solution

---
+++ src/GeekHub.Common/GeekHub.Common/DataAccess/IBaseRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace GeekHub.Common.DataAccess
+{
+    public interface IBaseRepository<TModel>
+    {
+        Task<TModel> GetAsync(Guid id);
+        
+        Task<IEnumerable<TModel>> GetAllAsync();
+        
+        Task<IEnumerable<TModel>> GetManyAsync(Expression<Func<TModel, bool>> predicate);
+
+        Task<TModel> CreateAsync(TModel model);
+        
+        TModel Update(TModel model);
+        
+        void Delete(Guid id);
+
+        Task SaveChangesAsync();
+    }
+}
