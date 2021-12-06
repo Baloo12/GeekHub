791de4cb20cf689e46f241a60c170873a668978d
Andrii Antosiev <40914830+Baloo12@users.noreply.github.com>
2021-12-06 12:02:14 +0200

    BggProvider. Move registrations to separate class (#45)
    #38

---
+++ src/GeekHub.BoardGames.BggProvider/GeekHub.BoardGames.BggProvider.Web/Registration/DomainServicesExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace GeekHub.BoardGames.BggProvider.Web.Registration
+{
+    using GeekHub.BoardGames.BggProvider.Domain.Api;
+    using GeekHub.BoardGames.BggProvider.Domain.Api.Http;
+    using GeekHub.BoardGames.BggProvider.Domain.Api.RequestParameters.Base;
+
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class DomainServicesExtensions
+    {
+        public static void RegisterBggApiCommunications(this IServiceCollection services)
+        {
+            services.AddTransient<IRequestParameterConstructor, RequestParameterConstructor>();
+            services.AddTransient<IRequestBuilderFactory, RequestBuilderFactory>();
+            services.AddTransient<IBggApiClient, BggXmlApiClient>();
+            services.AddTransient<IContentParser, XmlContentParser>();
+        }
+
+        public static void RegisterHttpClients(this IServiceCollection services)
+        {
+            services.AddHttpClient<IHttpClientHandler, HttpClientHandler>();
+        }
+    }
+}
