eca4de2e90caaae5a8181a3cb815172135c0d4d0
AlexAntosev <alexander.antosev@gmail.com>
2021-12-06 19:08:07 +0200

    Separate methods in enrich command

--- src/GeekHub.SteamProvider/GeekHub.SteamProvider.Domain/Commands/Handlers/VideoGames/EnrichVideoGameWithDetailsCommandHandler.cs
+++ src/GeekHub.SteamProvider/GeekHub.SteamProvider.Domain/Commands/Handlers/VideoGames/EnrichVideoGameWithDetailsCommandHandler.cs
@@ -1,9 +1,11 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using GeekHub.SteamProvider.Domain.Commands.VideoGames;
 using GeekHub.SteamProvider.Domain.Entities;
 using GeekHub.SteamProvider.Domain.Exceptions;
+using GeekHub.SteamProvider.Domain.Models.Internal;
 using GeekHub.SteamProvider.Domain.Queries.Developers;
 using GeekHub.SteamProvider.Domain.Queries.Genres;
 using GeekHub.SteamProvider.Domain.Queries.Platforms;
@@ -11,6 +13,7 @@ using GeekHub.SteamProvider.Domain.Queries.SteamApi;
 using GeekHub.SteamProvider.Domain.Queries.VideoGames;
 using GeekHub.SteamProvider.Domain.Utils;
 using MediatR;
+using Genre = GeekHub.SteamProvider.Domain.Entities.Genre;
 
 namespace GeekHub.SteamProvider.Domain.Commands.Handlers.VideoGames
 {
@@ -31,6 +34,19 @@ namespace GeekHub.SteamProvider.Domain.Commands.Handlers.VideoGames
             EnrichVideoGameWithDetailsCommand request,
             CancellationToken cancellationToken = default)
         {
+            var game = await GetVideoGameBySteamIdFromRepository(request, cancellationToken);
+            var details = await GetVideoGameDetailsFromSteam(request, cancellationToken);
+
+            var updatedGame = await EnirichVideoGameWithDetails(game, details, cancellationToken);
+
+            var updated = await UpdateVideoGame(updatedGame, cancellationToken);
+
+            return updated;
+        }
+
+        private async Task<VideoGame> GetVideoGameBySteamIdFromRepository(EnrichVideoGameWithDetailsCommand request,
+            CancellationToken cancellationToken)
+        {
             var queryGame = new QueryVideoGameBySteamId(request.SteamId);
             var game = await _mediator.Send(queryGame, cancellationToken);
 
@@ -39,6 +55,12 @@ namespace GeekHub.SteamProvider.Domain.Commands.Handlers.VideoGames
                 throw new VideoGameNotExistsException(request.SteamId);
             }
 
+            return game;
+        }
+        
+        private async Task<SteamGameDetails> GetVideoGameDetailsFromSteam(EnrichVideoGameWithDetailsCommand request,
+            CancellationToken cancellationToken)
+        {
             var queryDetails = new QueryVideoGameDetailsFromSteamApi(request.SteamId);
             var gameDetails = await _mediator.Send(queryDetails, cancellationToken);
 
@@ -47,27 +69,75 @@ namespace GeekHub.SteamProvider.Domain.Commands.Handlers.VideoGames
                 throw new VideoGameNotExistsInSteamException(request.SteamId);
             }
 
-            var queryDevelopers = new QueryOrCreateDevelopersByNames(gameDetails.Data?.Developers);
-            var developers = await _mediator.Send(queryDevelopers, cancellationToken);
-            
-            var queryGenres = new QueryOrCreateGenresByNames(gameDetails.Data?.Genres?.Select(g => g.Description).ToList());
-            var genres = await _mediator.Send(queryGenres, cancellationToken);
-            
-            var queryPlatforms = new QueryOrCreatePlatformsByNames(gameDetails.Data?.Platforms?.Where(p => p.Value).Select(p => p.Key).ToList());
-            var platforms = await _mediator.Send(queryPlatforms, cancellationToken);
+            return gameDetails;
+        }
+        
+        private async Task<VideoGame> EnirichVideoGameWithDetails(
+            VideoGame game,
+            SteamGameDetails details,
+            CancellationToken cancellationToken = default)
+        {
+            var developers = await GetDevelopersFromDetails(details, cancellationToken);
+            var genres = await GetGenresFromDetails(details, cancellationToken);
+            var platforms = await GetPlatformsFromDetails(details, cancellationToken);
 
             var builder = _videoGameEntityBuilderFactory.GetVideoGameEntityBuilder(game);
             var updatedGame = builder
-                .WithDetails(gameDetails.Data)
-                .WithSourceId(request.SteamId)
+                .WithDetails(details.Data)
                 .WithDevelopers(developers.ToList())
                 .WithGenres(genres.ToList())
                 .WithPlatforms(platforms.ToList())
                 .Build();
+            
+            return updatedGame;
+        }
+        
+        private async Task<IEnumerable<Developer>> GetDevelopersFromDetails(
+            SteamGameDetails gameDetails,
+            CancellationToken cancellationToken = default)
+        {
+            var developersNames = gameDetails.Data?.Developers;
+            
+            var queryDevelopers = new QueryOrCreateDevelopersByNames(developersNames);
+            var developers = await _mediator.Send(queryDevelopers, cancellationToken);
+            
+            return developers;
+        }
+        
+        private async Task<IEnumerable<Genre>> GetGenresFromDetails(
+            SteamGameDetails gameDetails,
+            CancellationToken cancellationToken = default)
+        {
+            var genresNames = gameDetails.Data?.Genres?
+                .Select(g => g.Description)
+                .ToList();
+            
+            var queryGenres = new QueryOrCreateGenresByNames(genresNames);
+            var genres = await _mediator.Send(queryGenres, cancellationToken);
+            
+            return genres;
+        }
 
+        private async Task<IEnumerable<Platform>> GetPlatformsFromDetails(
+            SteamGameDetails gameDetails,
+            CancellationToken cancellationToken = default)
+        {
+            var platformsNames = gameDetails.Data?.Platforms?
+                .Where(p => p.Value)
+                .Select(p => p.Key)
+                .ToList();
+            
+            var queryPlatforms = new QueryOrCreatePlatformsByNames(platformsNames);
+            var platforms = await _mediator.Send(queryPlatforms, cancellationToken);
+            
+            return platforms;
+        }
+        
+        private async Task<VideoGame> UpdateVideoGame(VideoGame updatedGame, CancellationToken cancellationToken)
+        {
             var updateCommand = new UpdateVideoGameCommand(updatedGame);
             var updated = await _mediator.Send(updateCommand, cancellationToken);
-
+            
             return updated;
         }
     }
