d9b338b273fdf762895b7a2969d7fcbaa0498dcd
Andrii Antosiev <gibson12ant@gmail.com>
2021-12-20 11:52:19 +0200

    Improve QueryPlaysByUserNameHandler to get all existing records.
    Add tests.

--- src/GeekHub.BoardGames.BggProvider/GeekHub.BoardGames.BggProvider.Domain/Queries/Handlers/QueryPlaysByUserNameHandler.cs
+++ src/GeekHub.BoardGames.BggProvider/GeekHub.BoardGames.BggProvider.Domain/Queries/Handlers/QueryPlaysByUserNameHandler.cs
@@ -1,5 +1,6 @@
 namespace GeekHub.BoardGames.BggProvider.Domain.Queries.Handlers
 {
+    using System;
     using System.Collections.Generic;
     using System.Threading;
     using System.Threading.Tasks;
@@ -9,11 +10,15 @@ namespace GeekHub.BoardGames.BggProvider.Domain.Queries.Handlers
     using GeekHub.BoardGames.BggProvider.Domain.Api;
     using GeekHub.BoardGames.BggProvider.Domain.Api.RequestParameters;
     using GeekHub.BoardGames.BggProvider.Domain.Dtos;
+    using GeekHub.BoardGames.BggProvider.Domain.Entities;
 
     using MediatR;
 
     public class QueryPlaysByUserNameHandler : IRequestHandler<QueryPlaysByUserNameRequest, IEnumerable<PlayRecordModel>>
     {
+        // IDEA: Move to constants or config
+        private const double PlaysPageSize = 100.0;
+
         private readonly IBggApiClient _bggApiClient;
 
         private readonly IMapper _mapper;
@@ -31,10 +36,44 @@ namespace GeekHub.BoardGames.BggProvider.Domain.Queries.Handlers
                     UserName = request.Username
                 };
 
-            var response = await _bggApiClient.GetPlayRecordsAsync(parameters);
-            var models = _mapper.Map<IEnumerable<PlayRecordModel>>(response.Plays);
+            var plays = await RequestPlays(parameters);
+
+            var models = _mapper.Map<IEnumerable<PlayRecordModel>>(plays);
 
             return models;
+        }
+
+        private async Task<List<PlayRecord>> RequestPlays(RequestPlaysParameters request)
+        {
+            var playRecords = new List<PlayRecord>();
+
+            var initialResponse = await _bggApiClient.GetPlayRecordsAsync(request);
+            playRecords.AddRange(initialResponse.Plays);
+
+            var pageCount = CalculatePageCount(initialResponse.TotalPlays);
+            playRecords.AddRange(await RequestRemainingPages(request, pageCount));
+
+            return playRecords;
+        }
+
+        private async Task<IEnumerable<PlayRecord>> RequestRemainingPages(RequestPlaysParameters request, double pageCount)
+        {
+            var result = new List<PlayRecord>();
+            while (request.Page < pageCount)
+            {
+                request.Page++;
+
+                var response = await _bggApiClient.GetPlayRecordsAsync(request);
+                
+                result.AddRange(response.Plays);
+            }
+
+            return result;
+        }
+
+        private static double CalculatePageCount(int totalPlays)
+        {
+            return Math.Ceiling(totalPlays / PlaysPageSize);
         }
     }
 }
